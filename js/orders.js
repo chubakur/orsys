569c102749ee8c98124eb57a05b4458186357fa5
Andrey Pitko <chubakur@gmail.com>
2015-09-30 21:56:29 +0300

    Добавил еще 2 контроллера ангуляра. Лента, логин, регистрация

--- js/orders.js
+++ js/orders.js
@@ -1,9 +1,30 @@
 var orsysApp = angular.module('orsysApp', []);
 
 orsysApp.controller('OrdersController', function ($scope, $interval, $log) {
+    $log.info("OrdersController init");
     $scope.loaded = true;
     function as(){
         $log.log(Date.now())
     };
     $interval(as, 5000);
+});
+
+orsysApp.controller('RegisterController', function ($scope, $log) {
+    $log.info("RegisterController init");
+    $scope.form = {
+        email: undefined,
+        password: undefined,
+        password2: undefined,
+        role: 'performer',
+    }
+    $scope.register = function () {
+        $log.log($scope.form);
+    }
+});
+
+orsysApp.controller('LoginController', function ($scope, $log){
+    $log.info("LoginController init");
+   $scope.login = function (){
+       $log.log("Login");
+   }
 });
