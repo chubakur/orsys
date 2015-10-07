d5fe0ffc78739aadafcb9368db4d6286c28fa91f
Andrey Pitko <chubakur@gmail.com>
2015-10-07 23:57:28 +0300

    Можем использовать last_insert_id вместо select. Потому что last_insert_id работает для курсора, поэтому race condition не будет!

--- register.php
+++ register.php
@@ -18,12 +18,12 @@ require('config.php');
 $db_params = $config['mysql']['users'];
 $connection = create_mysql_connection($db_params, $handlers);
 if(mysqli_query($connection, "INSERT INTO users (email, password, role) VALUES ('$email', '$cpassword', '$form_role')")){
-    $answ = mysqli_query($connection, "SELECT id FROM users WHERE email='$email'");
-    if(!$answ){
+    // можем использовать, потому что last_insert_id работает для курсора!
+    $user_id = mysqli_insert_id($connection);
+    if(!$user_id){
         end_script_immediately('{"status":"invalid"}', $handlers);
     }
-    $row = mysqli_fetch_assoc($answ);
-    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['user_id'] = $user_id;
     $_SESSION['email'] = $form_email;
     $_SESSION['role'] = $form_role;
     $_SESSION['csrf-token'] = generate_csrf_token($row['id']);
